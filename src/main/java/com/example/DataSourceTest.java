f40b77e7bdeb294c5fc7f23333a29cbe041a5718
oksk6685@handong.ac.kr <oksk6685@naver.com>
2023-12-10 18:35:05 +0900

    final

---
+++ src/main/java/com/example/DataSourceTest.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.support.FileSystemXmlApplicationContext;
+
+public class DataSourceTest {
+    @Test
+    public void testConnection(){
+        ApplicationContext ctx = new
+    }
+}
